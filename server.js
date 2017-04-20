dac5c74d80d262300ab38f3101c39ec38d37be06
Ethan Favia <efavia215@live.com>
2017-04-20 11:28:29 -0400

    more setup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// loads in the express library
 const express = require('express'); 
 
+// sets the app up 
 var app = express(); 
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>  {
     // res.send('<h1> Hello Express! </h1>');
@@ -18,4 +22,19 @@ app.get('/about', (req, res) => {
     res.send('About Page');
 });
 
-app.listen(3000);
+// bad send back json with error message
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'error handling request'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.send();
+});
+
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+});
